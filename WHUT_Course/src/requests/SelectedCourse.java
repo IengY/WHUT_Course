e3920003cee7f27492da2ebf1deec1ababe93e6d
lengyu <727109404@qq.com>
2018-03-17 00:06:01 +0800

    delete

--- WHUT_Course/src/requests/SelectedCourse.java
+++
@@ -1,9 +0,0 @@
-package requests;
-
-public class SelectedCourse {
-	public Course course;
-	public String addUrl;
-	public String message="NULL";
-	public int loop=0;
-	public boolean isAddSuccess=false;
-}
